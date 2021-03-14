c910a2c140f80f5bbffeb280f2c151c40cfafc52
Sebastian Kalicki <s.kalicki@kmedia.email>
2021-03-14 06:57:28 +0100

    Almost final version.

--- src/OpenGraphExtension.php
+++ src/OpenGraphExtension.php
@@ -5,12 +5,18 @@ namespace Kmedia\OpenGraph;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\ToggleCompositeField;
 use SilverStripe\ORM\DataExtension;
 
 class OpenGraphExtension extends DataExtension
 {
-    private static $db = [];
+    private static $db = [
+        'OGTitle' => 'Varchar(255)',
+        'OGDescription' => 'Text',
+        'OGType' => 'Varchar(255)',
+    ];
 
     private static $many_many = [
         'OGImage' => Image::class,
@@ -18,11 +24,41 @@ class OpenGraphExtension extends DataExtension
 
     public function updateCMSFields(FieldList $fields)
     {
+        $ogTitle = TextField::create('OGTitle', 'Title');
+        $ogTitle->setRightTitle('Page title goes here, automatically defaults to the page title');
+        $ogDescription = TextareaField::create('OGDescription', 'Description');
+        $ogDescription->setRightTitle('Page description goes here, automatically defaults to the content summary');
+        $ogType = TextField::create('OGType', 'Type');
         $ogImage = UploadField::create('OGImage', 'Image');
-        $ogImage->getValidator()->setAllowedExtensions(['png', 'jpeg', 'jpg', 'gif']);
+        $ogImage
+            ->setRightTitle('Ideal size: 1200px * 630px')
+            ->getValidator()
+            ->setAllowedExtensions(['png', 'jpeg', 'jpg', 'gif']);
 
         $fields->addFieldsToTab('Root.Main', [
-            ToggleCompositeField::create('OpenGraph', 'OpenGraph', [$ogImage])->setHeadingLevel(4)
+            ToggleCompositeField::create('OpenGraph', 'Open Graph', [$ogTitle, $ogDescription, $ogType, $ogImage])->setHeadingLevel(4)
         ]);
+    }
+
+    public function MetaTags(&$tags)
+    {
+        $type = trim($this->owner->OGType) ? $this->owner->OGType : 'website';
+        $title = trim($this->owner->OGTitle) ? $this->owner->OGTitle : $this->owner->Title;
+        $description = trim($this->owner->OGDescription) ? $this->owner->OGDescription : $this->owner->dbObject('Content')->FirstParagraph();
+
+        $tags .= sprintf('%2$s<meta name="og:type" content="%1$s">', $type, PHP_EOL);
+        $tags .= sprintf('%2$s<meta name="og:title" content="%1$s">', $title, PHP_EOL);
+        $tags .= sprintf('%2$s<meta name="og:url" content="%1$s">', $this->owner->AbsoluteLink(), PHP_EOL);
+        $tags .= sprintf('%2$s<meta name="og:description" content="%1$s">', $description, PHP_EOL);
+
+        foreach ($this->owner->OGImage() as $image) {
+            if ($image->exists()) {
+                $tags .= sprintf('%2$s<meta name="og:image" content="%1$s">', $image->AbsoluteURL, PHP_EOL);
+                $tags .= sprintf('%2$s<meta name="og:image:secure_url" content="%1$s">', $image->AbsoluteURL, PHP_EOL);
+                $tags .= sprintf('%2$s<meta name="og:image:type" content="%1$s">', $image->MimeType, PHP_EOL);
+                $tags .= sprintf('%2$s<meta name="og:image:width" content="%1$s">', $image->Width, PHP_EOL);
+                $tags .= sprintf('%2$s<meta name="og:image:height" content="%1$s">', $image->Height, PHP_EOL);
+            }
+        }
     }
 }
